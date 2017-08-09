7cc9ea853b438409aa3d91e8d6b4b94f0b88c1b6
Vladyslav_Dubinin <Vladyslav_Dubinin@epam.com>
2017-08-09 14:56:49 +0300

    Add achievement microservice module to project

---
+++ business-services/achievement-microservice/src/main/java/com/dubinin/feedbackportal/AchievementMicroserviceServerApplication.java
@@ -0,0 +1,20 @@
+package com.dubinin.feedbackportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan
+@EnableAutoConfiguration
+public class AchievementMicroserviceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchievementMicroserviceServerApplication.class, args);
+
+	}
+	
+}
